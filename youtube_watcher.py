1341a99b98f47ab48dc0f3c33abe5e0254a75623
SimonNagy <61780624+SimonNagy@users.noreply.github.com>
2023-05-30 14:11:31 +0200

    paging function

--- youtube_watcher.py
+++ youtube_watcher.py
@@ -5,11 +5,12 @@ import requests
 import json
 from config import config
 
-def fetch_playlist_items_page(google_api_key, youtube_playlist_id):
+def fetch_playlist_items_page(google_api_key, youtube_playlist_id, page_token=None):
     response = requests.get("https://www.googleapis.com/youtube/v3/playlistItems", params={
             "key": google_api_key,
             "playlistId": youtube_playlist_id,
             "part": "contentDetails",
+            "pageToken": page_token
         })
     
     # JSON parsing
@@ -23,7 +24,7 @@ def main():
     logging.info("START")
     google_api_key = config["google_api_key"]
     youtube_playlist_id = config["youtube_playlist_id"]
-    fetch_playlist_items_page(google_api_key, youtube_playlist_id)
+    fetch_playlist_items_page(google_api_key, youtube_playlist_id, "EAAaBlBUOkNBVQ")
     
 
     """
