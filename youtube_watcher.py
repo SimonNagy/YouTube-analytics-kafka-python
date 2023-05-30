b6e8f7121cac16241d70053fccbf951a10b85479
SimonNagy <61780624+SimonNagy@users.noreply.github.com>
2023-05-30 14:08:55 +0200

    :fetch_playlist_items_page: <= def

--- youtube_watcher.py
+++ youtube_watcher.py
@@ -5,17 +5,29 @@ import requests
 import json
 from config import config
 
+def fetch_playlist_items_page(google_api_key, youtube_playlist_id):
+    response = requests.get("https://www.googleapis.com/youtube/v3/playlistItems", params={
+            "key": google_api_key,
+            "playlistId": youtube_playlist_id,
+            "part": "contentDetails",
+        })
+    
+    # JSON parsing
+    payload = json.loads(response.text)
+    
+    logging.debug("GOT %s", payload)
+
+    return payload
+
 def main():
     logging.info("START")
     google_api_key = config["google_api_key"]
     youtube_playlist_id = config["youtube_playlist_id"]
+    fetch_playlist_items_page(google_api_key, youtube_playlist_id)
+    
 
-    response = requests.get("https://www.googleapis.com/youtube/v3/playlistItems", params={
-        "key": google_api_key,
-        "playlistId": youtube_playlist_id,
-        "part": "contentDetails",
-        "maxResults": 50  # Maximum number of results per page (adjust as needed)
-    })
+    """
+    When using the ::fetch_playlist_items_page:: this response is no longer needed
 
     if response.status_code == 200:
         data = response.json()
@@ -39,6 +51,8 @@ def main():
             logging.error("Page info not found in response data: %s", data)
     else:
         logging.error("Request failed with status code: %d", response.status_code)
+        
+    """
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
