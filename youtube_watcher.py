20509126b947785cc398387388705369352e4a41
SimonNagy <61780624+SimonNagy@users.noreply.github.com>
2023-05-30 13:10:45 +0200

    Update youtube_watcher.py

--- youtube_watcher.py
+++ youtube_watcher.py
@@ -1,8 +1,15 @@
+from http.client import responses
 import logging
 import sys
+import requests
+
 
 def main():
     logging.info("START")
+    
+    response = requests.get("https://www.googleapis.com/youtube/v3/playlists", params={})
+
+    logging.debug("GOT %s", response.content)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
