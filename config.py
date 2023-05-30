66e8f21b29de0fa98fd05f31862b2509a1fcd4a2
SimonNagy <61780624+SimonNagy@users.noreply.github.com>
2023-05-30 13:29:03 +0200

    youtube playlist id

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 config = {
-    "google_api_key": "AIzaSyDahe0aUsj2oyOth6j-sO9lUkqLVahYFu8"
+    "google_api_key": "AIzaSyDahe0aUsj2oyOth6j-sO9lUkqLVahYFu8",
+    "youtube_playlist_id": "PLQXro2VDjyIx8CGKwW9QQGoQdtPrLZDdN",
 }
--- youtube_watcher.py
+++ youtube_watcher.py
@@ -7,8 +7,12 @@ from config import config
 def main():
     logging.info("START")
     google_api_key = config["google_api_key"]
+    youtube_playlist_id = config["youtube_playlist_id"]
 
-    response = requests.get("https://www.googleapis.com/youtube/v3/playlists", params={"key": google_api_key,})
+    response = requests.get("https://www.googleapis.com/youtube/v3/playlists", params={
+        "key": google_api_key,
+        "id": youtube_playlist_id,
+        })
 
     logging.debug("GOT %s", response.content)
 
