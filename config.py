0ed0a9f3baa7511ff199983b8a9cd7ef6eb428d2
SimonNagy <61780624+SimonNagy@users.noreply.github.com>
2023-05-30 13:43:25 +0200

    .

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 config = {
     "google_api_key": "AIzaSyDahe0aUsj2oyOth6j-sO9lUkqLVahYFu8",
-    "youtube_playlist_id": "PLQXro2VDjyIx8CGKwW9QQGoQdtPrLZDdN",
+    "youtube_playlist_id": "PLr3-bEi1N4khKZm2u6Q8pOLO-WNZrKDHn",
 }
--- youtube_watcher.py
+++ youtube_watcher.py
@@ -2,6 +2,7 @@ from http.client import responses
 import logging
 import sys
 import requests
+import json
 from config import config
 
 def main():
@@ -9,12 +10,25 @@ def main():
     google_api_key = config["google_api_key"]
     youtube_playlist_id = config["youtube_playlist_id"]
 
-    response = requests.get("https://www.googleapis.com/youtube/v3/playlists", params={
+    response = requests.get("https://www.googleapis.com/youtube/v3/playlistItems", params={
         "key": google_api_key,
-        "id": youtube_playlist_id,
-        })
+        "playlistId": youtube_playlist_id,
+        "part": "snippet",
+        "maxResults": 50  # Maximum number of results per page (adjust as needed)
+    })
 
-    logging.debug("GOT %s", response.content)
+    if response.status_code == 200:
+        data = response.json()
+        if "items" in data:
+            items = data["items"]
+            for item in items:
+                video_id = item["snippet"]["resourceId"]["videoId"]
+                video_title = item["snippet"]["title"]
+                logging.debug("Video ID: %s, Video Title: %s", video_id, video_title)
+        else:
+            logging.error("Items key not found in response data: %s", data)
+    else:
+        logging.error("Request failed with status code: %d", response.status_code)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
