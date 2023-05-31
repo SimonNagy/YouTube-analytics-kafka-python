4a8bbfad11445b0d02ca0a7f402afc280a419406
SimonNagy <61780624+SimonNagy@users.noreply.github.com>
2023-05-31 14:09:26 +0200

    delelte_config

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 config = {
-    "google_api_key": "AIzaSyDahe0aUsj2oyOth6j-sO9lUkqLVahYFu8",
+    "google_api_key": "...",
     "youtube_playlist_id": "PLr3-bEi1N4khKZm2u6Q8pOLO-WNZrKDHn",
     "kafka": {
         "bootstrap.servers": "pkc-75m1o.europe-west3.gcp.confluent.cloud:9092",
         "security.protocol": "sasl_ssl",
         "sasl.mechanism": "PLAIN",
-        "sasl.username": "XYGARYCETAPFDP7Y",
-        "sasl.password": "BQ45EhR3feYqFQsv2GzN8zV4vcU4xUiVsuD22iEattHTzoxIfii2FxBWJVuQHbV9",
+        "sasl.username": "...",
+        "sasl.password": "...",
     },
     "schema_registry": {
         "url": "https://psrc-y5q2k.europe-west3.gcp.confluent.cloud",
-        "basic.auth.user.info": "K3NFWXAFJSDPNLXP:VeiKZe+dW+jn1EK9zFWtKZ4O6u95LSj+5wfwK3X3Fs4Pt9PAudNszTcNmqi4Yn1t",
+        "basic.auth.user.info": "...:...",
     }
 }
